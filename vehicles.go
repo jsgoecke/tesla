cb41582fe04578dace48713f3083a984d5f9dcb1
Jason Goecke <jason@goecke.net>
2016-04-23 21:18:54 -0700

    Added more comments for the auto generated docs

--- vehicles.go
+++ vehicles.go
@@ -2,6 +2,7 @@ package tesla
 
 import "encoding/json"
 
+// Represents the vehicle as returned from the Tesla API
 type Vehicle struct {
 	Color                  interface{} `json:"color"`
 	DisplayName            string      `json:"display_name"`
@@ -19,20 +20,25 @@ type Vehicle struct {
 	BackseatTokenUpdatedAt interface{} `json:"backseat_token_updated_at"`
 }
 
+// The response that contains the vehicle details from the Tesla API
 type VehicleResponse struct {
 	Response *Vehicle `json:"response"`
 	Count    int      `json:"count"`
 }
 
+// Represents the vehicles from an account, as you could have more than
+// one Tesla associated to your account
 type Vehicles []struct {
 	*Vehicle
 }
 
+// The response that contains the vehicles details from the Tesla API
 type VehiclesResponse struct {
 	Response Vehicles `json:"response"`
 	Count    int      `json:"count"`
 }
 
+// Fetches the vehicles associated to a Tesla account via the API
 func (c *Client) Vehicles() (Vehicles, error) {
 	vehiclesResponse := &VehiclesResponse{}
 	body, err := c.get(BaseURL + "/vehicles")
