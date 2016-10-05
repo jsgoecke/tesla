a4a50eee3ce4b4621dda81d0e7922db26338fe9a
Martin Bruse <zond@google.com>
2016-10-05 08:34:41 +0200

    Fixed typo. Accepting non-bools as IsFrontDefrosterOn (it happened to be an int, once?).

--- commands.go
+++ commands.go
@@ -125,7 +125,7 @@ func (v Vehicle) LockDoors() error {
 
 // Sets the temprature of the vehicle, where you may set the driver
 // zone and the passenger zone to seperate temperatures
-func (v Vehicle) SetTemprature(driver float64, passenger float64) error {
+func (v Vehicle) SetTemperature(driver float64, passenger float64) error {
 	driveTemp := strconv.FormatFloat(driver, 'f', -1, 32)
 	passengerTemp := strconv.FormatFloat(passenger, 'f', -1, 32)
 	apiUrl := BaseURL + "/vehicles/" + strconv.FormatInt(v.ID, 10) + "/command/set_temps?driver_temp=" + driveTemp + "&passenger_temp=" + passengerTemp
